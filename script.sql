2a0cc221a973aa8e2dce4c93cb619fb7d67b21c3
Lucas Silva <silva.pereira.92@gmail.com>
2023-10-14 16:26:16 -0300

    ap10(estruturas-repeticoes): nº valores positivos

--- script.sql
+++ script.sql
@@ -12,6 +12,41 @@ END $$;
 
 
 
+--exercicio 1.1
+
+DO
+$$
+DECLARE
+    numero1 NUMERIC := 7;
+    numero2 NUMERIC := -5;
+    numero3 NUMERIC := 6;
+    numero4 NUMERIC := -3.4;
+    numero5 NUMERIC := 4.6;
+    numero6 NUMERIC := 12;
+    total_positivos INT := 0;
+BEGIN
+    IF numero1 > 0 THEN
+        total_positivos := total_positivos + 1;
+    END IF;
+    IF numero2 > 0 THEN
+        total_positivos := total_positivos + 1;
+    END IF;
+    IF numero3 > 0 THEN
+        total_positivos := total_positivos + 1;
+    END IF;
+    IF numero4 > 0 THEN
+        total_positivos := total_positivos + 1;
+    END IF;
+    IF numero5 > 0 THEN
+        total_positivos := total_positivos + 1;
+    END IF;
+    IF numero6 > 0 THEN
+        total_positivos := total_positivos + 1;
+    END IF;
+
+    RAISE NOTICE 'Total de valores positivos: %', total_positivos;
+END;
+$$
 
 
 
